451e0f0fd42a838e7548746bba6ab0d61960c08e
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-08-06 20:18:36 -0300

    Feat: Barra de Navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
 
+import Nav from "./components/Nav";
+
 function App() {
   return (
     <div className="background">
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,29 @@
+import searchLogo from "../../assets/search.svg";
+import bellLogo from "../../assets/bell.svg";
+import userLogo from "../../assets/user.svg";
+import styles from "./Nav.module.css"
+
+export default function Nav() {
+    return(
+        <nav className={styles.navBar}>
+            <h1 className={styles.logo}>Kenai</h1>
+            <ul className="">
+                <li>
+                    <img src={searchLogo} alt="" />
+                </li>
+                <li>
+                    <a href="">Inicio</a>
+                </li>
+                <li>
+                    <a href="">Minha lista</a>
+                </li>
+                <li>
+                    <img src={bellLogo} alt="" />
+                </li>
+                <li>
+                    <img src={userLogo} alt="" />
+                </li>
+            </ul>
+        </nav>
+    );
+}
